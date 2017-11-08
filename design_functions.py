39bb2c6c946761aedf46d16bc10d23478a600045
jctay04 <33423608+jctay04@users.noreply.github.com>
2017-11-07 20:57:23 -0500

    Update design_functions.py

--- design_functions.py
+++ design_functions.py
@@ -1,42 +1,4 @@
-"""
-def draw_square(some_turtle):
 
-    for _ in range(4):
-        some_turtle.forward(200)
-        some_turtle.right(90)
-
-def draw_art():
-
-    # Turtle Brad
-    brad = Turtle(shape="turtle")
-    brad.color("yellow")
-    brad.pensize(2)
-    brad.speed("normal")  # 6/normal is the default so don't need to do it
-
-    for _ in range(36):
-        draw_square(brad)
-        brad.right(10)
-
-    # Turtle Angie
-    angie = Turtle(shape="turtle")
-    angie.color("blue")
-    angie.pensize(2)
-    angie.speed(5)  # slightly slower than brad
-
-    size = 1
-
-    for _ in range(300):
-        angie.forward(size)
-        angie.right(91)
-        size += 1
-
-window = Screen()
-window.bgcolor("black")
-
-draw_art()
-
-window.exitonclick()
-"""
 import turtle
 from random import randint
 
@@ -72,31 +34,11 @@ for times in range(50):
     taylor.color("orange")
     taylor.pensize(2)
     taylor.speed(0)
-
+    
+    #Turtle Chris
     Chris =turtle.Turtle()
     Chris.color("red")
     Chris.begin_fill()
     Chris.circle(50)
     Chris.end_fill()
     
-"""    
-def moveToRandomLocation():
-    turtle.penup()
-    turtle.setpos( randint(-400,400) , randint(-400,400) )
-    turtle.pendown()
-
-def draw_star(starSize, starColor):
-    turtle.color(starColor)
-    turtle.pendown()
-    begin_fill()
-    for times in range(5):
-        t.left(144)
-        t.forward(starSize)
-    end_fill()
-    penup()
-
-for star in range(30):
-    moveToRandomLocation()
-    drawStar( randint(5,25) , "blue")
- 
-""" 
